0b403be07330190a152fe63498c00122c55a45b1
marouanekhabbaz <marwane.khab@gmail.com>
2021-08-26 15:19:44 -0400

    first commit

---
+++ src/TryItOut.java
@@ -0,0 +1,30 @@
+public class TryItOut {
+    public static void main(String[] args){
+
+        double taxPercent = 8;
+        double tipPercent = 15;
+
+        double personOneAppetizerCost = 9.99;
+        double personOneMainCost = 23.99;
+        double personOneDessertCost = 10.29;
+        double personOneDrinkCost = 8.50;
+
+        double personTwoAppetizerCost = 12.99;
+        double personTwoMainCost = 18.99;
+        double personTwoDessertCost = 9.99;
+        double personTwoDrinkCost = 4.25;
+
+
+
+        String myName ="marouane";
+        String personOneName = "Adeline";
+        String personOneAppetizer = "Stuffed Ziti Fritta";
+        String personOneMain = "Shrimp Scampi";
+        String personOneDessert = "Sicilian Cheesecake";
+        String personOneDrink = "Raspberry Lemonade x 2";
+        double personOneSubTotal = personOneAppetizerCost + personOneMainCost
+                + personOneDessertCost + personOneDrinkCost;
+
+        System.out.println(personOneSubTotal);
+    }
+}
